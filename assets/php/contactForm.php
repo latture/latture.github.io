cca729d40409f282360b3582e36e7d064626fec7
Ryan Latture <ryan.latture@gmail.com>
2016-08-18 19:14:50 -0700

    Reformatted whitespace.

--- assets/php/contactForm.php
+++ assets/php/contactForm.php
@@ -9,19 +9,19 @@
     	$from    = $_POST['c_email'];
     	$message = $_POST['c_message'];
 
-		if (mail($to, $subject, $message, $from)) { 
+		if (mail($to, $subject, $message, $from)) {
 			$result = array(
 				'message' => 'Thanks for contacting us!',
 				'sendstatus' => 1
 				);
 			echo json_encode($result);
-		} else { 
+		} else {
 			$result = array(
 				'message' => 'Sorry, something is wrong',
 				'sendstatus' => 1
 				);
 			echo json_encode($result);
-		} 
+		}
 	}
 
 ?>
