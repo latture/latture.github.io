7199c16c2bef8eebe99efeefbacc9edc9b014def
Ryan Latture <ryan.latture@gmail.com>
2016-12-29 14:37:40 -0600

    Updated assets to version 1.4.2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -140,63 +140,47 @@
 		$(".video").fitVids();
 
 		/* ---------------------------------------------- /*
-		 * E-mail validation
-		/* ---------------------------------------------- */
-
-		function isValidEmailAddress(emailAddress) {
-			var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-			return pattern.test(emailAddress);
-		};
-
-		/* ---------------------------------------------- /*
 		 * Contact form ajax
 		/* ---------------------------------------------- */
 
-		$("#contact-form").submit(function(e) {
+		$('#contact-form').find('input,textarea').jqBootstrapValidation({
+			preventSubmit: true,
+			submitError: function($form, event, errors) {
+				// additional error messages or events
+			},
+			submitSuccess: function($form, event) {
+				event.preventDefault();
 
-			e.preventDefault();
+				var submit          = $('#contact-form submit');
+				var ajaxResponse    = $('#contact-response');
+				var name            = $('#contact-form [name="name"]').val();
+				var email           = $('#contact-form [name="email"]').val();
+				var message         = $('#contact-form [name="message"]').val();
 
-			var c_name = $("#c_name").val();
-			var c_email = $("#c_email").val();
-			var c_message = $("#c_message ").val();
-			var responseMessage = $('.ajax-response');
-
-			if (( c_name== "" || c_email == "" || c_message == "") || (!isValidEmailAddress(c_email) )) {
-				responseMessage.fadeIn(500);
-				responseMessage.html('<i class="fa fa-warning"></i> Check all fields.');
-			}
-
-			else {
 				$.ajax({
-					type: "POST",
-					url: "assets/php/contactForm.php",
+					type: 'POST',
+					url: 'assets/php/contact.php',
 					dataType: 'json',
 					data: {
-						c_email: c_email,
-						c_name: c_name,
-						c_message: c_message
+						name: name,
+						email: email,
+						message: message,
 					},
+					cache: false,
 					beforeSend: function(result) {
-						$('#contact-form button').empty();
-						$('#contact-form button').append('<i class="fa fa-cog fa-spin"></i> Wait...');
+						submit.empty();
+						submit.append('<i class="fa fa-cog fa-spin"></i> Wait...');
 					},
 					success: function(result) {
 						if(result.sendstatus == 1) {
-							responseMessage.html(result.message);
-							responseMessage.fadeIn(500);
-							$('#contact-form').fadeOut(500);
+							ajaxResponse.html(result.message);
+							$form.fadeOut(500);
 						} else {
-							$('#contact-form button').empty();
-							$('#contact-form button').append('<i class="fa fa-retweet"></i> Try again.');
-							responseMessage.html(result.message);
-							responseMessage.fadeIn(1000);
+							ajaxResponse.html(result.message);
 						}
 					}
 				});
 			}
-
-			return false;
-
 		});
 
 	});
